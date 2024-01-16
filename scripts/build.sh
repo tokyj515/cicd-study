4ceaff908569da8a1bbc0a2706dd24a358c74718
YuJin Kwon <tokyj515@naver.com>
2024-01-16 21:59:49 +0900

    feat: 배포 스크립트 수정13

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROJECT_NAME="app"
 JAR_PATH="/home/ubuntu/$PROJECT_NAME/build/libs/*.jar"
-DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME
+DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/cicd-0.0.1-SNAPSHOT.jar
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
 APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
